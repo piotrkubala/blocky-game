ada64c24258bed1b61c5e5e1a60ae66482857d06
Piotr Kubala <pkubala@student.agh.edu.pl>
2024-07-06 21:14:08 +0200

    representation builder

--- blocky_game/model/board_state.py
+++ blocky_game/model/board_state.py
@@ -6,6 +6,7 @@ from .game_objects import (
     GameBoard,
     GameObject,
     Colour,
+    Place,
     Room,
     Person,
     Thing,
@@ -34,35 +35,85 @@ def place_name_to_coordinates(place_name: str) -> tuple[int, int]:
     return row, column
 
 
-class BoardState:
-    def __pddl_type_to_object(self, pddl_type: str, *params) -> GameObject | None:
-        match pddl_type:
-            case "colour":
-                return Colour(*params)
-            case "room":
-                return Room(*params)
-            case "person":
-                return Person(*params)
-            case "thing":
-                return Thing(*params)
-            case "takeable":
-                return TakeableThing(*params)
-            case "key":
-                return Key(*params)
-            case "terminal":
-                return Terminal(*params)
-            case "exit":
-                return MapExit(*params)
-            case "entrance":
-                return Entrance(*params)
-            case _:
-                return None
+def direction_name_to_direction(direction_name: str) -> Direction:
+    match direction_name:
+        case "right":
+            return Direction.RIGHT
+        case "down":
+            return Direction.DOWN
+        case "left":
+            return Direction.LEFT
+        case "up":
+            return Direction.UP
+        case _:
+            raise ValueError(f"Invalid direction name {direction_name}")
 
-    def __generate_representation(self) -> GameBoard:
+
+def pddl_type_to_object(pddl_type: str, *params) -> GameObject | None:
+    match pddl_type:
+        case "colour":
+            return Colour(*params)
+        case "room":
+            return Room(*params)
+        case "person":
+            return Person(*params)
+        case "thing":
+            return Thing(*params)
+        case "takeable":
+            return TakeableThing(*params)
+        case "key":
+            return Key(*params)
+        case "terminal":
+            return Terminal(*params)
+        case "exit":
+            return MapExit(*params)
+        case "entrance":
+            return Entrance(*params)
+        case "place":
+            return Place(*params)
+        case "direction":
+            return None
+        case _:
+            raise ValueError(f"Invalid PDDL type {pddl_type}")
+
+
+def assert_game_object_type(game_object: GameObject, expected_type: str):
+    def get_assert_message(expected_type_name: str, got_game_object: GameObject):
+        return f"Expected {expected_type_name}, got {type(got_game_object)}"
+
+    match expected_type:
+        case "place":
+            assert isinstance(game_object, Place), get_assert_message(expected_type, game_object)
+        case "room":
+            assert isinstance(game_object, Room), get_assert_message(expected_type, game_object)
+        case "person":
+            assert isinstance(game_object, Person), get_assert_message(expected_type, game_object)
+        case "thing":
+            assert isinstance(game_object, Thing), get_assert_message(expected_type, game_object)
+        case "takeable":
+            assert isinstance(game_object, TakeableThing), get_assert_message(expected_type, game_object)
+        case "key":
+            assert isinstance(game_object, Key), get_assert_message(expected_type, game_object)
+        case "terminal":
+            assert isinstance(game_object, Terminal), get_assert_message(expected_type, game_object)
+        case "exit":
+            assert isinstance(game_object, MapExit), get_assert_message(expected_type, game_object)
+        case "entrance":
+            assert isinstance(game_object, Entrance), get_assert_message(expected_type, game_object)
+        case "direction":
+            assert isinstance(game_object, Direction), get_assert_message(expected_type, game_object)
+        case "colour":
+            assert isinstance(game_object, Colour), get_assert_message(expected_type, game_object)
+        case _:
+            raise ValueError(f"Invalid expected type {expected_type}")
+
+
+class BoardState:
+    def __generate_representation(self) -> tuple[GameBoard, dict[str, GameObject]]:
         max_row, max_column = 0, 0
 
         for obj in self.problem.objects:
-            if obj.type_name == "place":
+            if obj.type_tag == "place":
                 row, column = place_name_to_coordinates(obj.name)
                 max_row = max(max_row, row)
                 max_column = max(max_column, column)
@@ -71,28 +122,91 @@ class BoardState:
         game_objects = {}
 
         for obj in self.problem.objects:
-            game_object = self.__pddl_type_to_object(obj.type_name, obj.name)
+            game_object = pddl_type_to_object(obj.type_tag, obj.name)
 
             if game_object is None:
                 continue
 
             game_objects[obj.name] = game_object
 
+            if obj.type_tag == "place":
+                row, column = place_name_to_coordinates(obj.name)
+
+                assert_game_object_type(game_object, "place")
+                game_board[(row, column)] = game_object
+
         for relation in self.problem.init:
-            match relation.predicate:
+            match relation.name:
                 case "at":
-                    room_name, place_name = relation.args
-                    row, column = place_name_to_coordinates(place_name)
-                    game_board[(row, column)] = game_objects[room_name]
+                    room, place = relation.terms
+                    room_name, place_name = room.name, place.name
+
+                    assert_game_object_type(game_objects[room_name], "room")
+                    assert_game_object_type(game_objects[place_name], "place")
+                    game_objects[place_name].set_room(game_objects[room_name])
                 case "in":
-                    person_name, room_name = relation.args
+                    person, room = relation.terms
+                    person_name, room_name = person.name, room.name
+
+                    assert_game_object_type(game_objects[person_name], "person")
+                    assert_game_object_type(game_objects[room_name], "room")
                     game_objects[room_name].add_person(game_objects[person_name])
                 case "contains":
-                    room_name, thing_name = relation.args
-                    game_objects[room_name].add_thing(game_objects[thing_name])
-                # TODO: Add more cases here for other predicates
+                    room, thing = relation.terms
+                    room_name, thing_name = room.name, thing.name
 
-        raise NotImplementedError("Not implemented")
+                    assert_game_object_type(game_objects[room_name], "room")
+                    assert_game_object_type(game_objects[thing_name], "thing")
+                    game_objects[room_name].add_thing(game_objects[thing_name])
+                case "passage":
+                    room, entrance, direction = relation.terms
+                    room_name, entrance_name, direction_name = room.name, entrance.name, direction.name
+
+                    assert_game_object_type(game_objects[room_name], "room")
+                    assert_game_object_type(game_objects[entrance_name], "entrance")
+
+                    direction = direction_name_to_direction(direction_name)
+                    game_objects[room_name].add_entrance(
+                        game_objects[entrance_name], direction
+                    )
+                case "has_door":
+                    entrance, colour = relation.terms
+                    entrance_name, colour_name = entrance.name, colour.name
+
+                    assert_game_object_type(game_objects[entrance_name], "entrance")
+                    assert_game_object_type(game_objects[colour_name], "colour")
+
+                    game_objects[entrance_name].add_door(
+                        colour_name, game_objects[colour_name]
+                    )
+                case "owned":
+                    thing, person = relation.terms
+                    thing_name, person_name = thing.name, person.name
+
+                    assert_game_object_type(game_objects[thing_name], "takeable")
+                    assert_game_object_type(game_objects[person_name], "person")
+
+                    game_objects[person_name].add_equipment(game_objects[thing_name])
+                case "is":
+                    key, colour = relation.terms
+                    key_name, colour_name = key.name, colour.name
+
+                    assert_game_object_type(game_objects[key_name], "key")
+                    assert_game_object_type(game_objects[colour_name], "colour")
+
+                    game_objects[key_name].set_colour(game_objects[colour_name])
+                case "escaped":
+                    person = relation.terms[0]
+                    person_name = person.name
+
+                    assert_game_object_type(game_objects[person_name], "person")
+                    game_objects[person_name].escape()
+                case "reversed" | "free" | "adjacent":
+                    pass
+                case _:
+                    raise ValueError(f"Invalid predicate {relation.name}")
+
+        return game_board, game_objects
 
     def __init__(self, board_domain: BoardDomain, problem_definition_path: str):
         self.problem = pddl.parse_problem(problem_definition_path)
@@ -100,4 +214,4 @@ class BoardState:
 
         self.problem.check(self.domain.domain)
 
-        self.game_board = self.__generate_representation()
+        self.game_board, self.game_objects = self.__generate_representation()
--- blocky_game/model/game_objects.py
+++ blocky_game/model/game_objects.py
@@ -9,31 +9,6 @@ class Direction(Enum):
     LEFT = (0, -1)
 
 
-class GameBoard:
-    def __init__(self, rows: int, columns: int):
-        self.rows = rows
-        self.columns = columns
-        self.board = [[None for _ in range(columns)] for _ in range(rows)]
-
-    def __getitem__(self, key: tuple[int, int]):
-        row, column = key
-        return self.board[row][column]
-
-    def __setitem__(self, key: tuple[int, int], value):
-        row, column = key
-        self.board[row][column] = value
-
-    def get_neighbour(self, row: int, column: int, direction: Direction):
-        row_offset, column_offset = direction.value
-        new_row = row + row_offset
-        new_column = column + column_offset
-
-        assert 0 <= new_row < self.rows, f"Row {new_row} is out of bounds"
-        assert 0 <= new_column < self.columns, f"Column {new_column} is out of bounds"
-
-        return self.board[new_row][new_column]
-
-
 class GameObject:
     def __init__(self, name: str):
         self.name = name
@@ -44,29 +19,9 @@ class Colour(GameObject):
         super().__init__(name)
 
 
-class Person(GameObject):
-    def __init__(self, name: str):
-        super().__init__(name)
-        self.equipment: dict[str, TakeableThing] = {}
-
-
 class Thing(GameObject):
     def __init__(self, name: str):
         super().__init__(name)
-
-
-class Room(GameObject):
-    def __init__(self, name: str):
-        super().__init__(name)
-
-        self.people: dict[str, Person] = {}
-        self.things_contained: dict[str, Thing] = {}
-
-    def add_person(self, person: Person):
-        self.people[person.name] = person
-
-    def add_thing(self, thing: Thing):
-        self.things_contained[thing.name] = thing
 
 
 class TakeableThing(Thing):
@@ -94,6 +49,83 @@ class MapExit(Thing):
         super().__init__(name)
 
 
+class Person(GameObject):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.equipment: dict[str, TakeableThing] = {}
+        self.escaped: bool = False
+
+    def escape(self):
+        self.escaped = True
+
+    def add_equipment(self, equipment: TakeableThing):
+        self.equipment[equipment.name] = equipment
+
+
 class Entrance(GameObject):
     def __init__(self, name: str):
         super().__init__(name)
+
+        self.doors: dict[str, Colour] = {}
+
+    def add_door(self, door_name: str, colour: Colour):
+        self.doors[door_name] = colour
+
+
+class Room(GameObject):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+        self.people: dict[str, Person] = {}
+        self.things_contained: dict[str, Thing] = {}
+        self.entrances: dict[Direction, Entrance] = {}
+
+    def add_person(self, person: Person):
+        self.people[person.name] = person
+
+    def add_thing(self, thing: Thing):
+        self.things_contained[thing.name] = thing
+
+    def add_entrance(self, entrance: Entrance, direction: Direction):
+        self.entrances[direction] = entrance
+
+
+class Place(GameObject):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+        self.room: Room | None = None
+
+    def set_room(self, room: Room):
+        self.room = room
+
+    def unset_room(self):
+        self.room = None
+
+
+class GameBoard:
+    def __init__(self, rows: int, columns: int):
+        self.rows: int = rows
+        self.columns: int = columns
+        self.board: list[list[None | Place]] = [[None for _ in range(columns)] for _ in range(rows)]
+
+    def __getitem__(self, key: tuple[int, int]) -> Place | None:
+        row, column = key
+        return self.board[row][column]
+
+    def __setitem__(self, key: tuple[int, int], value: Place | None):
+        row, column = key
+        self.board[row][column] = value
+
+    def get_neighbour(self, row: int, column: int, direction: Direction) -> Place | None:
+        row_offset, column_offset = direction.value
+        new_row = row + row_offset
+        new_column = column + column_offset
+
+        is_valid_row = 0 <= new_row < self.rows
+        is_valid_column = 0 <= new_column < self.columns
+
+        if not is_valid_row or not is_valid_column:
+            return None
+
+        return self.board[new_row][new_column]
